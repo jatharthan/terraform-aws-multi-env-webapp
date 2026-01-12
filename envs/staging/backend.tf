aa6825ff8a53b386ed850d714d5d3d27d34d80d7
Jatharthan Tharmanandasivam <jathu10061994@gmail.com>
2026-01-12 10:37:24 -0500

    Change S3 bucket name for staging backend

--- envs/staging/backend.tf
+++ envs/staging/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "boot4-tf-state-ncpl-jatha"
+    bucket         = "boot4-tf-state-jatha"
     key            = "staging/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "boot4-terraform-lock-table"
